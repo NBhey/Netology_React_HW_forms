8a8e1d67873442ca03ec4438a154694a642d34e9
Oganes <og.marckaryan@yandex.ru>
2025-03-24 20:54:25 +0300

    add rgb text

--- src/Components/HexToRgbConverter.jsx
+++ src/Components/HexToRgbConverter.jsx
@@ -4,7 +4,15 @@ import React, { useState } from "react";
 //#DC143C
 //#9ACD32
 const HexToRgbConverter = () => {
-  let [colorInputHex, setColorInputHex] = useState('#34495e');
+  let [colorInputHex, setColorInputHex] = useState("#34495e");
+
+  const r = parseInt(colorInputHex.substring(1, 3), 16);
+  const g = parseInt(colorInputHex.substring(3, 5), 16);
+  const b = parseInt(colorInputHex.substring(5, 7), 16);
+  let [colorRGB, setColorRGB] = useState(`rgb ${r}, ${g}, ${b}`);
+
+  
+  
 
   const main = {
     backgroundColor: colorInputHex,
@@ -15,23 +23,22 @@ const HexToRgbConverter = () => {
     display: "flex",
     flexDirection: "column",
     justifyContent: "center",
-    transition: 'background-color 0.3s'
+    transition: "background-color 0.3s",
   };
-  
+
   const onInputChange = (e) => {
-    if(e.target.value.length === 7){
-        console.log('достигло 7')
-        setColorInputHex(e.target.value) 
+    if (e.target.value.length === 7) {
+      console.log("достигло 7");
+      setColorInputHex(e.target.value);
+      setColorRGB(`rgb ${r}, ${g}, ${b}`);
     }
-     
   };
-  
 
   return (
     <main style={main}>
       <div>
-        <input type="text" maxLength="7" onInput={onInputChange} />
-        <p>1234</p>
+        <input type="text" maxLength="7" onInput={onInputChange} placeholder={colorInputHex} />
+        <p>{colorRGB}</p>
       </div>
     </main>
   );
