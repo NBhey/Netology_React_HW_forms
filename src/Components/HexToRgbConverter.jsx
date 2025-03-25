5a0079f20f60ac3ecb99139fc8aa7b2cc3d5c3d3
Oganes <og.marckaryan@yandex.ru>
2025-03-25 10:30:14 +0300

    hw_forms_ hex2rgb finish

--- src/Components/HexToRgbConverter.jsx
+++ src/Components/HexToRgbConverter.jsx
@@ -5,14 +5,7 @@ import React, { useState } from "react";
 //#9ACD32
 const HexToRgbConverter = () => {
   let [colorInputHex, setColorInputHex] = useState("#34495e");
-
-  const r = parseInt(colorInputHex.substring(1, 3), 16);
-  const g = parseInt(colorInputHex.substring(3, 5), 16);
-  const b = parseInt(colorInputHex.substring(5, 7), 16);
-  let [colorRGB, setColorRGB] = useState(`rgb ${r}, ${g}, ${b}`);
-
-  
-  
+  let [colorRGB, setColorRGB] = useState(`rgb(52, 73, 94)`);
 
   const main = {
     backgroundColor: colorInputHex,
@@ -26,18 +19,34 @@ const HexToRgbConverter = () => {
     transition: "background-color 0.3s",
   };
 
+  const onInputSuccessfully = (e) => {
+    setColorInputHex(e);
+    const r = parseInt(e.substring(1, 3), 16);
+    const g = parseInt(e.substring(3, 5), 16);
+    const b = parseInt(e.substring(5, 7), 16);
+    setColorRGB((colorRGB = `rgb(${r}, ${g}, ${b})`));
+  };
+
   const onInputChange = (e) => {
-    if (e.target.value.length === 7) {
-      console.log("достигло 7");
-      setColorInputHex(e.target.value);
-      setColorRGB(`rgb ${r}, ${g}, ${b}`);
+    colorInputHex = e.target.value;
+    if (colorInputHex.length === 7) {
+      if (/^#[0-9A-Fa-f]{6}$/.test(colorInputHex)) {
+        onInputSuccessfully(colorInputHex);
+      } else {
+        setColorRGB(`Ошибка!`);
+      }
     }
   };
 
   return (
     <main style={main}>
       <div>
-        <input type="text" maxLength="7" onInput={onInputChange} placeholder={colorInputHex} />
+        <input
+          type="text"
+          maxLength="7"
+          onInput={onInputChange}
+          placeholder={colorInputHex}
+        />
         <p>{colorRGB}</p>
       </div>
     </main>
