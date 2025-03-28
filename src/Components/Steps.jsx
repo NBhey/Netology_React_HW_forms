d0b26fca2141362a06d2d4f3624df7772cb3417c
Oganes <og.marckaryan@yandex.ru>
2025-03-28 13:20:07 +0300

    hw_forms_steps finish

--- src/Components/Steps.jsx
+++ src/Components/Steps.jsx
@@ -3,37 +3,53 @@ import React, { useRef, useState } from "react";
 const Steps = () => {
   const dateInput = useRef();
   const walkInput = useRef();
-  // localStorage.setItem("list", JSON.stringify([]));
-  // console.log(localStorage.getItem('list'))
+
   let [dataForSave, setDataForSave] = useState([]);
 
   const onSubmit = (e) => {
     e.preventDefault();
-    console.log("Начало",dataForSave)
 
     const currentDate = dateInput.current.value.split("-").reverse().join(".");
     const currentWalk = +walkInput.current.value;
+
     let saveNewData;
 
     const existingDate = dataForSave.findIndex(
       (element) => element.currentDate === currentDate
     );
-    
-    if(existingDate === -1){
-      saveNewData = [...dataForSave, {currentDate, currentWalk}]
-      setDataForSave(saveNewData)
-    } else {
 
-      saveNewData = dataForSave.map((el)=>{
-        if(el.currentDate === currentDate){
-          el.currentWalk += currentWalk
-        } return el
-      })
-      setDataForSave(saveNewData)
+    if (existingDate === -1) {
+      saveNewData = [...dataForSave, { currentDate, currentWalk }];
+
+      saveNewData.sort((a, b) => {
+        const parseDate = (dateStr) => {
+          const [day, month, year] = dateStr.split(".").map(Number);
+          return new Date(year, month - 1, day).getTime();
+        };
+
+        const timestampA = parseDate(a.currentDate);
+        const timestampB = parseDate(b.currentDate);
+
+        return timestampB - timestampA;
+      });
+
+      setDataForSave(saveNewData);
+    } else {
+      saveNewData = dataForSave.map((el) => {
+        if (el.currentDate === currentDate) {
+          el.currentWalk += currentWalk;
+        }
+        return el;
+      });
+      setDataForSave(saveNewData);
     }
-    console.log("Конец",dataForSave )
   };
 
+  const handleDelete = (dateToDelete) => {
+    setDataForSave(
+      dataForSave.filter((item) => item.currentDate !== dateToDelete)
+    );
+  };
   return (
     <>
       <form onSubmit={onSubmit} className="Steps-form" action="">
@@ -60,7 +76,9 @@ const Steps = () => {
         </thead>
         <tbody>
           {dataForSave.map((el, i) => {
-            return <StepsTable key={i} dataForSave={el} />;
+            return (
+              <StepsTable key={i} dataForSave={el} onDelete={handleDelete} />
+            );
           })}
         </tbody>
       </table>
@@ -68,15 +86,15 @@ const Steps = () => {
   );
 };
 
-const StepsTable = ({ dataForSave }) => {
-  console.log('я StepsTable', dataForSave)
+const StepsTable = ({ dataForSave, onDelete }) => {
   return (
     <tr>
-      <th> {dataForSave.currentDate} </th>
-      <th> {dataForSave.currentWalk} </th>
-      <th>
-        <span>ред</span> <button>x</button>
-      </th>
+      <td>{dataForSave.currentDate}</td>
+      <td>{dataForSave.currentWalk}</td>
+      <td>
+        <span>ред</span>
+        <button onClick={() => onDelete(dataForSave.currentDate)}>x</button>
+      </td>
     </tr>
   );
 };
